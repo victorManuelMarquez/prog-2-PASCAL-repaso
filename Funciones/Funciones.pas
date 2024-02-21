a3d33e495b820983e91554c4a899301bd0795a3d
victor <victormmla3lm@gmail.com>
2024-02-20 22:36:39 -0300

    Funciones.

--- Funciones/Funciones.pas
+++ Funciones/Funciones.pas
@@ -1,6 +1,24 @@
 program Funciones;
 uses crt;
-function saludo(nombre : string) : string;
+function potencia(base, exp : Integer) : Integer;
+var
+    resultado, i : Integer; // variables locales
 begin
-    clrscr;
+    resultado := 1;
+    for i := 1 to exp do
+    begin
+        resultado := resultado * base;
+    end;
+    potencia := resultado; // asigno la vaariable "resultado" como valor de retorno.
+end;
+var
+    base, exponente : Integer;
+begin
+    ClrScr;
+    write('Ingrese la base: ');
+    readln(base);
+    write('Ingrese la potencia: ');
+    readln(exponente);
+    writeln('Resultado: ', potencia(base, exponente));
+    writeln('Fin del programa.');
 end.
